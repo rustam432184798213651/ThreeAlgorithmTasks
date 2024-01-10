12d5aafba63a306ad7586dcf03024a496de4afe4
Radzhabov Rustam <rrradzhabov@edu.hse.ru>
2024-01-10 17:41:38 +0300

    Create function to determine if the string is balanced in terms of open and closed brackets

--- ThreeAlgorithmTasks/ThreeAlgorithmTasks.cpp
+++ ThreeAlgorithmTasks/ThreeAlgorithmTasks.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <string>
 
 int FibonacciNumber(int n)
 {
@@ -15,20 +17,33 @@ int FibonacciNumber(int n)
     return FibonacciNumber(n - 1) + FibonacciNumber(n - 2);
 }
 
+// Check if the string is balanced in terms of open and closed brackets
+bool isBalancedString(std::string str)
+{
+    std::stack<char> Stack;
+    for (auto el : str)
+    {
+        if (el == '(')
+        {
+            Stack.push(el);
+        }
+        else if (el == ')')
+        {
+            if (Stack.size() == 0)
+            {
+                return false;
+            }
+            else
+            {
+                Stack.pop();
+            }
+        }
+    }
+    return Stack.size() == 0;
+}
+
 
 int main()
 {
-    
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
